7b07d2574a31bc1866469e99c2d7c2e3251edfba
Konkina Alina <konkinaan@student.bmstu.ru>
2023-12-19 11:50:28 +0300

    last improve rpz

--- report/generate_table/gen.py
+++ report/generate_table/gen.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib
-import numpy
+import numpy as np
+from scipy import linalg
 
 TEST_MAX_LENGTH = 24
 TEST_COUNT = 20
@@ -33,8 +34,8 @@ def plot_time():
     matplotlib.rcParams.update({'figure.figsize': (10, 7)})
     matplotlib.rcParams.update({'axes.prop_cycle': matplotlib.cycler(color=['black'])})
 
-    plt.plot(values_l, values_t[0], linestyle='solid', label='Отсуствие рекурсивной части алгоритма')
-    plt.plot(values_l, values_t[1], linestyle='dashed', label='Выполнение рекурсивной части алгоритма')
+    plt.plot(values_l, values_t[0], 'o', label='Коэффициент отражения равен 0')
+    plt.plot(values_l, values_t[1], 'v', label='Коэффицинт отражения не равен 0')
 
     plt.xlabel('Количество фигур на сцене')
     plt.ylabel(r'Процессорное время (тики)')
@@ -50,6 +51,50 @@ def plot_time():
 
     plt.show()
 
+    val_t_1 = np.array(values_t[0])
+    val_t_2 = np.array(values_t[1])
+    val_x = np.array(values_l)
+    # m = len(val_x)
+    # X1 = np.stack((np.ones(m), val_x, val_x**8)).T
+    # X2 = np.stack((np.ones(m), val_x, val_x**6)).T
+    # beta1, resuaials1, _, _ = linalg.lstsq(X1, val_t_1)
+    # beta2, residuals2, rank2, sv2 = linalg.lstsq(X2, val_t_2)
+    # print(resuaials1)
+    # print(residuals2)
+    # print(beta1)
+    # print(beta2)
+
+    cof1, residuals1, _, _, _ = np.polyfit(val_x, val_t_1, 6, full=True)
+    print(cof1)
+    print(residuals1)
+    p1 = np.poly1d(cof1)
+    y_t_1 = p1(val_x)
+
+    cof2, residuals2, _, _, _ = np.polyfit(val_x, val_t_2, 10, full=True)
+    print(cof2)
+    print(residuals2)
+    p2 = np.poly1d(cof2)
+    y_t_2 = p2(val_x)
+
+    # plt.plot(values_l, X1@beta1, label='Апроксимация при коэффициенте отражения равному 0')
+    # plt.plot(values_l, X2@beta2, linestyle='--', label='Апроксимация при при коэффициенте отражения не равному 0')
+    # plt.plot(values_l, values_t[0], 'o', label='Отсуствие рекурсивной части алгоритма')
+    # plt.plot(values_l, values_t[1], 'v', label='Выполнение рекурсивной части алгоритма')
+
+    plt.plot(values_l, y_t_1, label='Апроксимация при коэффициенте отражения равному 0')
+    plt.plot(values_l, y_t_2, linestyle='--', label='Апроксимация при при коэффициенте отражения не равному 0')
+    plt.plot(values_l, values_t[0], 'o', label='Отсуствие рекурсивной части алгоритма')
+    plt.plot(values_l, values_t[1], 'v', label='Выполнение рекурсивной части алгоритма')
+    ax = plt.gca()
+    ax.margins(0.001)
+
+    plt.legend(fontsize=18)
+
+    # plt.loglog()
+    plt.xlabel('Количество фигур на сцене')
+    plt.ylabel(r'Процессорное время (тики)')
+    plt.tight_layout()
+    plt.show()
 
 # def plot_mem():
 #     plt.plot(values_l, values_m[1], linestyle='solid', label='Сортировка Шелла')
